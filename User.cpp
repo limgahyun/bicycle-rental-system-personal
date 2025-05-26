4faa9f19f6799640c060ef88270e7d87d7bf06fb
limgahyun <limgh2002@gmail.com>
2025-05-27 02:14:17 +0900

    feat: User

--- User.cpp
+++ User.cpp
@@ -6,22 +6,52 @@ User::User() : isLoggedIn(false) {}
 
 User::User(string id, string password) : userId(id), password(password), userRole(USER), isLoggedIn(false) {}
 
+/*
+	함수 이름 : User::findUserById()
+	기능	 : 해당 id를 가진 user 찾기
+	전달 인자 : id
+	반환값    : 해당 id를 가진 user 찾기 성공 여부
+*/
 bool User::findUserById(string searchId) {
     return this->userId == searchId;
 }
 
+/*
+	함수 이름 : User::checkPassword()
+	기능	 : 해당 password가 일치하는지 확인
+	전달 인자 : password
+	반환값    : 해당 password가 일치하는지 여부
+*/
 bool User::checkPassword(string inputPassword) {
     return this->password == inputPassword;
 }
 
+/*
+	함수 이름 : User::setLoggedIn()
+	기능	 : 로그인 상태 변경
+	전달 인자 : 로그인 상태
+	반환값    : 없음
+*/
 void User::setLoggedIn(bool status) {
     this->isLoggedIn = status;
 }
 
+/*
+	함수 이름 : User::getLoggedIn()
+	기능	 : 로그인 상태 반환
+	전달 인자 : 없음
+	반환값    : 로그인 상태
+*/
 bool User::getLoggedIn() const {
     return isLoggedIn;
 }
 
+/*
+	함수 이름 : User::getId()
+	기능	 : 해당 user의 id 반환
+	전달 인자 : 없음
+	반환값    : 해당 user의 id
+*/
 string User::getId() const {
     return userId;
 } 
